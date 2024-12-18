b20f7de4afd52f7879f0ab1a8ead717b144d4610
KvitselV <113378172+KvitselV@users.noreply.github.com>
2024-12-18 19:22:49 +0300

    .

--- lab1/shortClass_IS.py
+++
@@ -1,7 +0,0 @@
-class ShortSoiskatel(Soiskatel):
-    def __init__(self, fam, imya, otchestvo):
-        super().__init__(fam, imya, otchestvo, kvalifikaciya="N/A", professiya="N/A",
-                         data_rozhdeniya=date(1900, 1, 1), telefon="0000000000", adres="N/A")
-
-    def __str__(self):
-        return f"ShortSoiskatel: {self.get_fam()} {self.get_imya()[0]}. {self.get_otchestvo()[0] if self.get_otchestvo() else ''}."
